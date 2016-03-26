6f30c3175e351d471e4903eed7fdf7f9853fe167
wumarco <marco.wyx@gmail.com>
2016-03-26 18:52:48 +0800

    first commit for project

---
+++ src/main/java/com/ebill/ElectronicbillApplication.java
@@ -0,0 +1,12 @@
+package com.ebill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicbillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicbillApplication.class, args);
+	}
+}
